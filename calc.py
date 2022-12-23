f11fc14896bd83223ac28bbdfb6ec67ff686d62d
RioGruz <90642491+RioGruz@users.noreply.github.com>
2022-12-23 19:48:01 +0100

    drugi drugi

--- calc.py
+++ calc.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import streamlit as st 
 import numpy as np
 
@@ -22,29 +21,4 @@ def main():
 
 
 if __name__ == '__main__':
-=======
-import streamlit as st 
-import numpy as np
-
-def main():
-
-	st.title("Demo web kalkulator EUR")
-
-	with st.form(key='myform', clear_on_submit = True):
-		cijena_u_EUR = st.number_input("Molimo unesite cijenu ulaznice u EUR", step=1.00)
-		cijena_u_KN = float(7.53450) * float(cijena_u_EUR)
-		primljeno_u_KN = st.number_input("Molimo unesite iznos primljen u KUNAMA", step=10.00)
-		povrat_u_KN = float(primljeno_u_KN) - float(cijena_u_KN)
-		povrat_u_EUR = float(povrat_u_KN)/float(7.53450)
-		
-		submit_button = st.form_submit_button("Unesi podatke")
-
-		if submit_button:
-			st.header('Iznos povrata u EUR je:', )
-			st.header(np.round(float(povrat_u_EUR),2))
-			
-
-
-if __name__ == '__main__':
->>>>>>> c7cf38dda02b8ff450c05a9523355e488cdb858b
 	main()
